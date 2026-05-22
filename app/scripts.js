a20c10f4e65c48438922e173bc71021458de3220
Alex Authors <78132024+aauthors@users.noreply.github.com>
2026-05-22 12:26:30 +1000

    create new page with clock for pomo

--- app/scripts.js
+++ app/scripts.js
@@ -1,22 +1,38 @@
-var string = "hello world"; 
+var string = "hello world";
 
-document.getElementById("box").innerHTML = string;
+var box = document.getElementById("box");
+if (box) {
+    box.innerHTML = string;
+}
 
 function changeText() {
-    document.getElementById("box").innerHTML = "I love you";
+    var b = document.getElementById("box");
+    if (b) b.innerHTML = "I love you";
 }
 
 function changeNoText() {
-    document.getElementById("box").innerHTML = "I understand, but I still love you";
+    var b = document.getElementById("box");
+    if (b) b.innerHTML = "I understand, but I still love you";
+}
+
+function goToPomo() {
+    window.location.href = "./pomo.html";
+}
+
+function goToHomePage() {
+    window.location.href = "./index.html";
 }
 
 function updateClock() {
-    var clock = document.getElementById("clock"); 
-    var now = new Date(); 
-    var hours = now.getHours().toString().padStart(2, '0'); 
+    var clock = document.getElementById("clock");
+    if (!clock) return;
+    var now = new Date();
+    var hours = now.getHours().toString().padStart(2, '0');
     var minutes = now.getMinutes().toString().padStart(2, '0');
     var seconds = now.getSeconds().toString().padStart(2, '0');
-    clock.innerHTML = hours + ":" + minutes + ":" + seconds; 
+    clock.innerHTML = hours + ":" + minutes + ":" + seconds;
 }
 
+// show time immediately and then update every second
+updateClock();
 setInterval(updateClock, 1000);
