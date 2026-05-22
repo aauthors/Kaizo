7d434f1a89ac91438722efccad79effb731f3cd3
Alex Authors <78132024+aauthors@users.noreply.github.com>
2026-05-22 11:19:35 +1000

    added in a function that updates the time

--- app/scripts.js
+++ app/scripts.js
@@ -4,9 +4,19 @@ document.getElementById("box").innerHTML = string;
 
 function changeText() {
     document.getElementById("box").innerHTML = "I love you";
-    alert("I love you");
 }
 
 function changeNoText() {
-    alert("I understand, but I still love you");
+    document.getElementById("box").innerHTML = "I understand, but I still love you";
 }
+
+function updateClock() {
+    var clock = document.getElementById("clock"); 
+    var now = new Date(); 
+    var hours = now.getHours().toString().padStart(2, '0'); 
+    var minutes = now.getMinutes().toString().padStart(2, '0');
+    var seconds = now.getSeconds().toString().padStart(2, '0');
+    clock.innerHTML = hours + ":" + minutes + ":" + seconds; 
+}
+
+setInterval(updateClock, 1000);
